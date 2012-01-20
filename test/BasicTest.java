64fc2f61473277864826ec8291448dd51ad1eeb0
Geoffrey Parker <Professional.Cynic@gmail.com>
2012-01-19 18:09:38 -0600

    added and tested comments

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -57,4 +57,35 @@ public class BasicTest extends UnitTest {
 		assertEquals("Hello world", firstPost.content);
 		assertNotNull(firstPost.postedAt);
 	}
+	
+	@Test
+	public void postComments() {
+		// Create a new user and save it
+		User bob = new User("bob@gmail.com", "secret", "Bob").save();
+	 
+		// Create a new post
+		Post bobPost = new Post(bob, "My first post", "Hello world").save();
+	 
+		// Post a first comment
+		new Comment(bobPost, "Jeff", "Nice post").save();
+		new Comment(bobPost, "Tom", "I knew that !").save();
+	 
+		// Retrieve all comments
+		List<Comment> bobPostComments = Comment.find("byPost", bobPost).fetch();
+	 
+		// Tests
+		assertEquals(2, bobPostComments.size());
+	 
+		Comment firstComment = bobPostComments.get(0);
+		assertNotNull(firstComment);
+		assertEquals("Jeff", firstComment.author);
+		assertEquals("Nice post", firstComment.content);
+		assertNotNull(firstComment.postedAt);
+	 
+		Comment secondComment = bobPostComments.get(1);
+		assertNotNull(secondComment);
+		assertEquals("Tom", secondComment.author);
+		assertEquals("I knew that !", secondComment.content);
+		assertNotNull(secondComment.postedAt);
+	}
 }
