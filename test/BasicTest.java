5d56274a31e57a99aefb67cbc11c57a44d9da47e
Geoffrey Parker <Professional.Cynic@gmail.com>
2012-01-19 18:06:43 -0600

    wrote and ran unit test for post

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,6 +5,11 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
+	@Before
+	public void setup () {
+		Fixtures.deleteDatabase();
+	}
+
 	@Test
 	public void createAndRetrieveUser() {
 		// Create a new user and save it
@@ -27,5 +32,29 @@ public class BasicTest extends UnitTest {
 		assertNotNull(User.connect("bob@gmail.com", "secret"));
     	assertNull(User.connect("bob@gmail.com", "badpassword"));
 	    assertNull(User.connect("tom@gmail.com", "secret"));
+	}
+	
+	@Test
+	public void createPost() {
+		// Create a new user and save it
+		User bob = new User("bob@gmail.com", "secret", "Bob").save();
+		
+		// Create a new post
+		new Post(bob, "My first post", "Hello world").save();
+		
+		// Test that the post has been created
+		assertEquals(1, Post.count());
+		
+		// Retrieve all posts created by Bob
+		List<Post> bobPosts = Post.find("byAuthor", bob).fetch();
+		
+		// Tests
+		assertEquals(1, bobPosts.size());
+		Post firstPost = bobPosts.get(0);
+		assertNotNull(firstPost);
+		assertEquals(bob, firstPost.author);
+		assertEquals("My first post", firstPost.title);
+		assertEquals("Hello world", firstPost.content);
+		assertNotNull(firstPost.postedAt);
 	}
 }
